c5af0625130ce5b02a8bb94ced957656854d85e8
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2023-04-22 12:02:21 +0200

    Make argument optional

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
+const { exec } = require("child_process");
 
 program.command('connect')
   .description('Connects to the machine')
-  .argument('<project-name>', 'Project name (for example: nl-cod2-test, nl-cod2-zom)')
+  .argument('[project-name]', 'Project name (for example: nl-cod2-test, nl-cod2-zom)', null)
   .action((projectName) => {
     console.log(projectName);
   });
