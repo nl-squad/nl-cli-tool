7b26f5895bb8861a3045ddba5f7b4c16bf5caaa6
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2023-04-22 12:17:58 +0200

    Implement connect

--- mynl.sh
+++ mynl.sh
@@ -7,8 +7,14 @@ function print_usage()
     echo "Usage: nl.sh"
 }
 
+function exec_ssh()
+{
+    ssh -i ~/.ssh/nl -t ubuntu@mynl.pl "$1"
+}
+
 if [[ $1 == "connect" ]]; then
     echo "Connecting to mynl.pl SSH"
+    exec_ssh "cd ~/cod2/servers/$project ; bash --login"
 else
     echo "Error: Invalid verb '$1'"
     print_usage
